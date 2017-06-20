72a2a494730e2880ff7a03f42f62a85e041a503b
CaioCris <caiohcris@gmail.com>
2017-06-20 16:25:32 -0300

    first commit

---
+++ adivinhacao.py
@@ -0,0 +1,35 @@
+print('********************************')
+print('Bem vindo ao jogo de Adivinhação')
+print('********************************')
+
+numero_secreto = 42
+rodada = 1
+total_de_tentativas = 3
+
+for rodada in range (1, total_de_tentativas + 1):
+    print('Você esta na rodada {} de {} tentatias'.format(rodada, total_de_tentativas))
+    chute = int(input('Digite um numero entre 1 e 100: '))
+
+    if chute < 1 or chute > 100:
+        print('Seu numero deve ser entre 1 ou 100!')
+        continue
+
+    print('Você digitou', chute)
+
+    #Variaveis com as condiçõs
+    acertou = numero_secreto == chute
+    maior = chute > numero_secreto
+    menor = chute < numero_secreto
+
+    if acertou:
+        print('Você acertou!')
+        break
+    else:
+        if maior:
+            print('Errou! Você chutou muito alto!')
+        elif menor:
+            print ('Errou! Você chutou muito baixo')
+
+    rodada = rodada + 1
+
+    print('Fim do jogo')

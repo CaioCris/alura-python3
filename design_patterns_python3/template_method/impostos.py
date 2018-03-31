7465570db5d02ca235c1a7c58e3ddcee495e1f67
CaioCris <caiohcris@gmail.com>
2018-03-31 20:39:13 -0300

    Aula 03 finalizada - Designer patterns part. 01

---
+++ design_patterns_python3/template_method/impostos.py
@@ -0,0 +1,65 @@
+from abc import ABCMeta, abstractmethod
+
+
+class TemplateImposto:
+    __metaclass__ = ABCMeta
+
+    def calcula(self, orcamento):
+        if self.deve_taixar(orcamento):
+            return self.maxima_taixa(orcamento)
+        else:
+            return self.minima_taixa(orcamento)
+
+    @abstractmethod
+    def deve_taixar(self, orcamento):
+        pass
+
+    @abstractmethod
+    def maxima_taixa(self, orcamento):
+        pass
+
+    @abstractmethod
+    def minima_taixa(self, orcamento):
+        pass
+
+
+class ISS:
+
+    def calcula(self, orcamento):
+        return orcamento.valor * 0.1
+
+
+class ICMS:
+
+    def calcula(self, orcamento):
+        return orcamento.valor * 0.06
+
+
+class ICPP(TemplateImposto):
+
+    def deve_taixar(self, orcamento):
+        return orcamento.valor > 500
+
+    def maxima_taixa(self, orcamento):
+        return orcamento.valor * 0.07
+
+    def minima_taixa(self, orcamento):
+        return orcamento.valor * 0.05
+
+
+class IKCV(TemplateImposto):
+
+    def deve_taixar(self, orcamento):
+        return orcamento.valor > 500 and self.__item_maior_100(orcamento)
+
+    def maxima_taixa(self, orcamento):
+        return orcamento.valor * 0.1
+
+    def minima_taixa(self, orcamento):
+        return orcamento.valor * 0.06
+
+    def __item_maior_100(self, orcamento):
+        for item in orcamento.obter_itens():
+            if item.valor > 100:
+                return True
+        return False

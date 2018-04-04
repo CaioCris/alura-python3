e6b8202f4c97fc7f18e2a90b7694d2eb9dfbb6b2
CaioCris <caiohcris@gmail.com>
2018-04-04 17:39:44 -0300

    Aula O4 finalizada - Design Patterns part. 02

--- design_patterns_python3/visitor/operacoes.py
+++ design_patterns_python3/visitor/operacoes.py
@@ -1,21 +1,45 @@
-class Soma:
-
-    def __init__(self, esquerda, direita):
-        self.__esquerda = esquerda
-        self.__direita = direita
-
-    def avalia(self):
-        return self.__esquerda.avalia() + self.__direita.avalia()
-
-
 class Subtracao:
 
-    def __init__(self, esquerda, direita):
-        self.__esquerda = esquerda
-        self.__direita = direita
+    def __init__(self, expressao_esquerda, expressao_direita):
+        self.__expressao_esquerda = expressao_esquerda
+        self.__expressao_direita = expressao_direita
 
     def avalia(self):
-        return self.__esquerda.avalia() - self.__direita.avalia()
+        return (self.__expressao_esquerda.avalia()
+                - self.__expressao_direita.avalia())
+
+    @property
+    def expressao_esquerda(self):
+        return self.__expressao_esquerda
+
+    @property
+    def expressao_direita(self):
+        return self.__expressao_direita
+
+    def aceita(self, visitor):
+        visitor.visita_subtracao(self)
+
+
+class Soma:
+
+    def __init__(self, expressao_esquerda, expressao_direita):
+        self.__expressao_esquerda = expressao_esquerda
+        self.__expressao_direita = expressao_direita
+
+    def avalia(self):
+        return (self.__expressao_esquerda.avalia()
+                + self.__expressao_direita.avalia())
+
+    def aceita(self, visitor):
+        visitor.visita_soma(self)
+
+    @property
+    def expressao_esquerda(self):
+        return self.__expressao_esquerda
+
+    @property
+    def expressao_direita(self):
+        return self.__expressao_direita
 
 
 class Numero:
@@ -26,9 +50,16 @@ class Numero:
     def avalia(self):
         return self.__numero
 
+    def aceita(self, visitor):
+        visitor.visita_numero(self)
+
 
 if __name__ == '__main__':
-    esquerda = Soma(Numero(10), Numero(20))
-    direita = Soma(Numero(5), Numero(2))
-    conta = Soma(esquerda, direita)
-    print(conta.avalia())
+    from design_patterns_python3.visitor.impressao import Impressao
+
+    expressao_esquerda = Subtracao(Numero(10), Numero(5))
+    expressao_direita = Soma(Numero(2), Numero(10))
+    expressao_conta = Soma(expressao_esquerda, expressao_direita)
+
+    visitor = Impressao()
+    expressao_conta.aceita(visitor)
